eb0fdaeb7d011f0a1489a956904a09b5661cd81c
GideonCaspi <gideoncaspi@gmail.com>
2019-04-18 17:30:02 +0100

    Added footer to buyer insights

--- buyer-insights/src/app/app.module.ts
+++ buyer-insights/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MosaicIconsComponent } from './components/mosaic-icons/mosaic-icons.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MosaicIconsComponent
+    MosaicIconsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
