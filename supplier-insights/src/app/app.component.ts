2424d24f16e6ade36414b4259c1bb5fefe91b4e0
haimei.zhang <haimeizhang2015@gmail.com>
2019-04-18 17:40:48 +0100

    Add sidebar toggle animation

--- supplier-insights/src/app/app.component.ts
+++ supplier-insights/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'supplier-insights';
+  sidebarToggled = false;
+
+  toggleSidebar(): void {
+    this.sidebarToggled = !this.sidebarToggled;
+  }
 }
