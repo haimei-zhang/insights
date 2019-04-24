e8318be150c353dd53a2eb4ac660a4474ae0d9ba
haimei.zhang <haimeizhang2015@gmail.com>
2019-04-24 17:28:22 +0100

    Add top categories products and customers

--- supplier-insights/src/app/components/top-customers/top-customers.component.ts
+++ supplier-insights/src/app/components/top-customers/top-customers.component.ts
@@ -12,4 +12,32 @@ export class TopCustomersComponent implements OnInit {
   ngOnInit() {
   }
 
+  initTopCustomersByOrderValueChart(): any {
+    return {
+      title: 'Top Customers by Order Value',
+      height: '400px',
+      chartId: 'top-customers-by-order-value-chart',
+      yAxis: ['test1','test2','test3','test4','test5','all'],
+      series: [18203, 23489, 29034, 104970, 131744, 630230]
+    };
+  }
+
+  initTopCustomersByOrderLinesChart(): any {
+    return {
+      title: 'Top Customers by Purchase Orders',
+      height: '400px',
+      chartId: 'top-customers-by-purchase-orders-chart',
+      yAxis: ['test1','test2','test3','test4','test5','all'],
+      series: [18203, 23489, 29034, 104970, 131744, 630230]
+    };
+  }
+
+  changePage(page, execute): void {
+    console.log(page);
+  }
+
+  setPageSize(page): void {
+    console.log(page)
+  }
+
 }
--- supplier-insights/src/app/components/top-products/top-products.component.ts
+++ supplier-insights/src/app/components/top-products/top-products.component.ts
@@ -12,4 +12,32 @@ export class TopProductsComponent implements OnInit {
   ngOnInit() {
   }
 
+  initTopProductsByOrderValueChart(): any {
+    return {
+      title: 'Top Products by Order Value',
+      height: '400px',
+      chartId: 'top-products-by-order-value-chart',
+      yAxis: ['test1','test2','test3','test4','test5','all'],
+      series: [18203, 23489, 29034, 104970, 131744, 630230]
+    };
+  }
+
+  initTopProductsByOrderLinesChart(): any {
+    return {
+      title: 'Top Products by Order Lines',
+      height: '400px',
+      chartId: 'top-products-by-order-lines-chart',
+      yAxis: ['test1','test2','test3','test4','test5','all'],
+      series: [18203, 23489, 29034, 104970, 131744, 630230]
+    };
+  }
+
+  changePage(page, execute): void {
+    console.log(page);
+  }
+
+  setPageSize(page): void {
+    console.log(page)
+  }
+
 }
