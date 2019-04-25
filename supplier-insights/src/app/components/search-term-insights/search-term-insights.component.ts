d3b5c5e4978269a47eb01d0fdc7baf3d6eed7b2b
haimei.zhang <haimeizhang2015@gmail.com>
2019-04-25 10:38:12 +0100

    Add search term insights page

--- supplier-insights/src/app/components/search-term-insights/search-term-insights.component.ts
+++ supplier-insights/src/app/components/search-term-insights/search-term-insights.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchTermInsightsComponent implements OnInit {
 
+  minHeight = '210px';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  initWeeklySearchesChart(): any {
+    return {
+      title: 'Weekly Searches containing "Pen"',
+      chartId: 'weekly-searches-chart',
+      height: '400px',
+      legend: ['All Searches', 'Searches that returned your products', 'Searches where your product was first'],
+      xAxis: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      series: [{
+        name: 'All Searches',
+        stack: 'total',
+        areaStyle: {},
+        data: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        type: 'line'
+      },
+        {
+          name: 'Searches that returned your products',
+          data: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+          areaStyle: {},
+          stack: 'total',
+          type: 'line'
+        },
+        {
+          name: 'Searches where your product was first',
+          data: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+          areaStyle: {},
+          stack: 'total',
+          type: 'line'
+        }]
+    };
   }
 
 }
